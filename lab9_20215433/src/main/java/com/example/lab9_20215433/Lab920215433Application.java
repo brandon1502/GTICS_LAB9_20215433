dd3a25e87027adf2f8fb3bd00e889e8509d4458b
brandon1502 <brandon16in.m@gmail.com>
2024-11-11 12:15:11 -0500

    inicio

---
+++ lab9_20215433/src/main/java/com/example/lab9_20215433/Lab920215433Application.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20215433;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab920215433Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab920215433Application.class, args);
+    }
+
+}
---
+++ lab9_20215433/src/test/java/com/example/lab9_20215433/Lab920215433ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20215433;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab920215433ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
